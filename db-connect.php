04209c9c013978cde3baa7a6b1a071d1307f497b
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2024-01-19 11:39:34 +0900

    Create db-connect.php

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1518109-final';
+    const USER = 'LAA1518109';
+    const PASS = 'Naoto2003';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
