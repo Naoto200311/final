e5d41f7ae04a791888115d044c5febb61bae3d9f
Naoto200311 <148735731+Naoto200311@users.noreply.github.com>
2024-01-19 11:39:45 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+<link rel="stylesheet" type="text/css" href="css/style.css?jlljj">
+<title>お土産システム</title>
+<body class="head">
+    <div class="has-text-centered">
+    <a href="final.php" class="head"><p class="is-size-2">お土産システム</p></a>
+    </div>
